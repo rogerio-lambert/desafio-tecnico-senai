a488dd02aa03d16b05bfd341cd74146f3ab1739f
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-15 13:12:37 -0300

    feat (structure code): create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Device from './pages/Device';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/device" component={ Device } />
+        <Route path="/Dashboard" component={ Dashboard } />
+        <Route component={ NotFound } />        
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
