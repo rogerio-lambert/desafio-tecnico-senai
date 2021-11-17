a310a319e7b902bc1625cea5442637e80e7b7809
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-17 17:26:51 -0300

    feat (home): create home component screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ButtonHome from '../components/ButtonHome';
+import Divider from '@mui/material/Divider';
+import RouterIcon from '@mui/icons-material/Router';
+import DvrIcon from '@mui/icons-material/Dvr';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
 
 function Home() {
-  const history = useHistory();
   return (
-    <section>
-      home
-      <button
-        type="button"
-        onClick={() => history.push('/device')}
+    <Box sx={{ width: '100%', display: 'flex', height: '450px', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <Typography
+        sx={{ width: '100%', maxWidth: 360, textAlign: 'center', marginBottom: '100px'}}
+        variant="h4"
       >
-        Criar um dispositivo
-      </button>
-      <button
-        type="button"
-        onClick={() => history.push('/dashboard')}
-      >
-        Criar um dashboard
-      </button>
-    </section>
+        Benvindos ao app de desmonstração de tecnologia socket.io/React
+      </Typography> 
+      <Box sx={{ width: '100%', maxWidth: 360 }}>
+        <Divider />
+        <nav aria-label="main mailbox folders">
+          <Fade in={true}>
+            <List>
+              <ButtonHome
+                title="Dispositivo"
+                href="/device"
+                icon={<RouterIcon />}
+              />
+              <Divider />
+              <ButtonHome
+                title="Dashboard"
+                href="/dashboard"
+                icon={<DvrIcon />}
+              />
+            </List>
+          </Fade>
+        </nav>
+        <Divider />
+      </Box>
+    </Box>
   );
 }
 
