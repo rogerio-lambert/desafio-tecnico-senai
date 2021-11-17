75e91ddc9b6d79a7b4b5d878cc6b57019520cf7d
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-17 17:34:39 -0300

    refactor (device): refact socket listeners and main component

--- frontend/src/pages/Device.jsx
+++ frontend/src/pages/Device.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import SwitchOnOff from '../components/SwitchOnOff';
+import ParameterConsole from '../components/ParameterConsole';
 
 const { io } = require("socket.io-client");
 require('dotenv').config();
 
+const {
+  createIdListener,
+  switchOnOffListener,
+  controlDeviceListener } = require('../utils/deviceListeners');
+
 const socketServerUrl = process.env.SOCKET_SERVER || 'http://localhost:3001';
-console.log(socketServerUrl)
 const socket = io(socketServerUrl);
 
 function Device(props) {
@@ -21,76 +29,77 @@ function Device(props) {
   const gain = [35.32, 64.47];
 
   useEffect(() => {
-    socket.emit('newDevice', { params, controls, offMode });
+    socket.emit('getId');
   }, []);
 
   useEffect(() => {
-    socket.on('createId', ({ id }) => setDeviceId(id));
-    socket.on('switchOnOff', ({ id, offMode: newPosition }) => {
-      id === deviceId && switchOffMode(newPosition);
-    });
-    socket.on('controlDevice', ({ newControls, id }) => {
-      if (id === deviceId) {
-        setControls(newControls);
-        socket.emit('updateDashboard', { id: deviceId, params, controls })
-      }
-    });
-  }, [controls, deviceId, offMode, params]);
+    createIdListener(socket, setDeviceId, params, controls, offMode);
+  }, []);
+
+  useEffect(() => {
+    switchOnOffListener(socket, deviceId, switchOffMode);
+  }, [offMode]);
+
+  useEffect(() => {
+    controlDeviceListener(socket, deviceId, setControls, gain, params, setParameters);
+  }, [controls, deviceId]);
+
+  const handleSwichOnOff = ({ target: { checked }}) => {
+    socket.emit('switchOnOff', { id: deviceId, offMode: !offMode })
+    switchOffMode(!checked);
+  };
 
   return (
-    <section>
-      <h1>{ `Dispositivo id: ${deviceId}` }</h1> 
-      {
-        Object.entries(controls).map((control, index) => {
-          const [paramName, paramValue] = Object.entries(params)[index]
-          const [controlName, controlValue]  =  control;
-          return (<fieldset htmlFor="" key={index}>
-              <legend>{paramName}</legend>
-              <label htmlFor="value">value</label>
-              <input 
-                type="text"
-                name="value"
-                id=""
-                value={offMode ? '---' : paramValue}
+    <Grid 
+      container
+      spacing={2}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ width: '100%', margin: '50px'}}
+    >
+      <Grid 
+        item
+        xs={5}
+        sx={{ width: '100%', maxWidth: 500, backgroundColor: '#f5f5f0', padding: '30px', borderRadius: '10px', border: 'black 1px solid'}}
+      >
+        <Typography
+          sx={{ width: '100%' , textAlign: 'left', marginBottom: '10px'}}
+          variant="h5"
+          // color="white"
+        >
+          { `Dispositivo id: ${deviceId}` }
+        </Typography> 
+        {
+          Object.entries(controls).map((control, index) => {
+            const [paramName, paramValue] = Object.entries(params)[index]
+            const [controlName, controlValue]  =  control;
+            const handleSlideControl = ({ target }) => {
+              const newControls = { ...controls, [controlName]: target.value }
+              setControls(newControls);
+              const newParams = { ...params, [paramName]:  gain[index] * target.value }
+              setParameters({ ...params, [paramName]:  gain[index] * target.value });
+              socket.emit('updateDashboard', { id: deviceId, params: newParams, controls: newControls });
+            }
+            return (
+              < ParameterConsole 
+                key={index}
+                handleSlideControl={handleSlideControl}
+                offMode={offMode}
+                paramName={paramName}
+                paramValue={paramValue}
+                controlValue={controlValue}
               />
-              <label htmlFor="control">control</label>
-              <input 
-                type="number"
-                name="control"
-                id=""
-                value={Object.values(controls)[index]}
-                disabled={offMode}
-                onChange={({ target }) => {
-                  setParameters({ ...params, [paramName]:  gain[index] * controlValue });
-                  setControls({ ...controls, [controlName]: target.value });
-                  socket.emit('updateDashboard', { id: deviceId, params, controls })
-                }}
-              />
-            </fieldset>)
-        })
-      }
-      <label htmlFor="on-off">
-        ligar/desligar       
-        <input 
-          type="checkbox"
-          name="on-off" 
-          id=""
-          checked={!offMode}
-          onChange={({ target: { checked }}) => {
-            socket.emit('switchOnOff', { id: deviceId, offMode: !offMode })
-            switchOffMode(!checked);
-          }}
-        />
-      </label>
-
-             
-    </section>
+            )
+          })
+        }
+        <SwitchOnOff 
+          offMode={offMode}
+          onChange={handleSwichOnOff}
+        /> 
+      </Grid>
+    </Grid>
   );
 }
 
 export default Device;
-
-// const [param1, setParam1] = useState(0);
-// const [param2, setParam2] = useState(0);
-// const [control1, setControl1] = useState(0);
-// const [control2, setcontrol2] = useState(0);
